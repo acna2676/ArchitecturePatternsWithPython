f4333d04a6abe018299e64e8a11e78a4cc0c96cd
Nakaishi Ryosuke <acna2676@gmail.com>
2022-11-13 15:57:40 +0900

    feat:chapter6

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -1,26 +1,40 @@
-import config
-import service_layer.services as services
-from allocation.adapters import orm, repository
+from datetime import datetime
+
+from allocation.adapters import orm
 from allocation.domain import model
+from allocation.service_layer import services, unit_of_work
 from flask import Flask, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-orm.start_mappers()
-get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
+orm.start_mappers()
+
+
+@app.route("/add_batch", methods=["POST"])
+def add_batch():
+    eta = request.json["eta"]
+    if eta is not None:
+        eta = datetime.fromisoformat(eta).date()
+    services.add_batch(
+        request.json["ref"],
+        request.json["sku"],
+        request.json["qty"],
+        eta,
+        unit_of_work.SqlAlchemyUnitOfWork(),
+    )
+    return "OK", 201
 
 
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
-    line = model.OrderLine(
-        request.json["orderid"], request.json["sku"], request.json["qty"],
-    )
-
     try:
-        batchref = services.allocate(line, repo, session)
+        batchref = services.allocate(
+            request.json["orderid"],
+            request.json["sku"],
+            request.json["qty"],
+            unit_of_work.SqlAlchemyUnitOfWork(),
+        )
     except (model.OutOfStock, services.InvalidSku) as e:
         return {"message": str(e)}, 400
 
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,7 +1,6 @@
-import allocation.domain.model as model
-import allocation.service_layer.services as services
 import pytest
 from allocation.adapters import repository
+from allocation.service_layer import services, unit_of_work
 
 
 class FakeRepository(repository.AbstractRepository):
@@ -18,36 +17,42 @@ class FakeRepository(repository.AbstractRepository):
         return list(self._batches)
 
 
-class FakeSession:
-    committed = False
+class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
+    def __init__(self):
+        self.batches = FakeRepository([])
+        self.committed = False
 
     def commit(self):
         self.committed = True
 
-
-def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    repo = FakeRepository([batch])
-
-    result = services.allocate(line, repo, FakeSession())
-    assert result == "b1"
+    def rollback(self):
+        pass
 
 
-def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
-    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
-    repo = FakeRepository([batch])
+def test_add_batch():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
+    assert uow.batches.get("b1") is not None
+    assert uow.committed
+
+
+def test_allocate_returns_allocation():
+    uow = FakeUnitOfWork()
+    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
+    assert result == "batch1"
+
+
+def test_allocate_errors_for_invalid_sku():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "AREALSKU", 100, None, uow)
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate(line, repo, FakeSession())
+        services.allocate("o1", "NONEXISTENTSKU", 10, uow)
 
 
-def test_commits():
-    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
-    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
-    repo = FakeRepository([batch])
-    session = FakeSession()
-
-    services.allocate(line, repo, session)
-    assert session.committed is True
+def test_allocate_commits():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
+    assert uow.committed
