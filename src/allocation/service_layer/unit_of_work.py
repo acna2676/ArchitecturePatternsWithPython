06c5236bd01baa6d97192525844be020d573191e
Nakaishi Ryosuke <acna2676@gmail.com>
2022-11-15 17:42:22 +0900

    feat: chapter7

--- src/allocation/service_layer/unit_of_work.py
+++ src/allocation/service_layer/unit_of_work.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm.session import Session
 
 
 class AbstractUnitOfWork(abc.ABC):
-    batches: repository.AbstractRepository  # FIXME Why is this need? Test passed if not exist.
+    products: repository.AbstractRepository
 
     def __enter__(self) -> AbstractUnitOfWork:
         return self
@@ -31,6 +31,7 @@ class AbstractUnitOfWork(abc.ABC):
 DEFAULT_SESSION_FACTORY = sessionmaker(
     bind=create_engine(
         config.get_postgres_uri(),
+        isolation_level="REPEATABLE READ",
     )
 )
 
@@ -41,8 +42,54 @@ class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
 
     def __enter__(self):
         self.session = self.session_factory()  # type: Session
-        self.batches = repository.SqlAlchemyRepository(self.session)
-        return super().__enter__()  # FIXME Why is this need? Test passed if not exist.
+        self.products = repository.SqlAlchemyRepository(self.session)
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()  # pylint: disable=attribute-defined-outside-init
+
+
+class AbstractUnitOfWork(abc.ABC):
+    products: repository.AbstractRepository
+
+    def __enter__(self) -> AbstractUnitOfWork:
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @abc.abstractmethod
+    def commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
+
+
+DEFAULT_SESSION_FACTORY = sessionmaker(
+    bind=create_engine(
+        config.get_postgres_uri(),
+        isolation_level="REPEATABLE READ",
+    )
+)
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session = self.session_factory()  # type: Session
+        self.products = repository.SqlAlchemyRepository(self.session)
+        return super().__enter__()
 
     def __exit__(self, *args):
         super().__exit__(*args)
