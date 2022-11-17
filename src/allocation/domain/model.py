a1be511828b006c10fc5090559b5438c169a3c6b
Nakaishi Ryosuke <acna2676@gmail.com>
2022-11-17 20:35:57 +0900

    feat: chapter10

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from datetime import date
 from typing import List, Optional, Set
 
-from . import events
+from . import commands, events
 
 
 class Product:
@@ -29,9 +29,7 @@ class Product:
         batch._purchased_quantity = qty
         while batch.available_quantity < 0:
             line = batch.deallocate_one()
-            self.events.append(
-                events.AllocationRequired(line.orderid, line.sku, line.qty)
-            )
+            self.events.append(commands.Allocate(line.orderid, line.sku, line.qty))
 
 
 @dataclass(unsafe_hash=True)
